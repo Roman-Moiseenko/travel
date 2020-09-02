65b78bd334187842985f69ef90989e18ff6b7eee
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2020-09-02 15:21:56 +0200

    8.Review Complite!

--- frontend/widgets/ReviewsToursWidget.php
+++ frontend/widgets/ReviewsToursWidget.php
@@ -8,16 +8,42 @@ use yii\base\Widget;
 
 class ReviewsToursWidget extends Widget
 {
-
     public $tours;
 
-//TODO сДЕЛАТЬ Подгрузку отзывов
     public function run()
     {
         $reviews = $this->tours->reviews;
         if (count($reviews) == 0) return;
+        $array = [];
+        foreach ($reviews as $review) {
+            $vote = RatingWidget::widget(['rating' => $review->vote]);
+            $fullname = $review->user->personal->fullname->getFullname();
+            $date = date('d-m-Y', $review->created_at);
+            $text = $review->text;
+            $array[] = <<<HTML
+<div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div>
+                                    $vote
+                                </div>
+                                <div class="select-text">
+                                     $fullname
+                                </div>
+                                <div class="ml-auto">
+                                    $date
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="p-3">
+                                $text
+                            </div>
+                        </div>
+                    </div>
+HTML;
+        }
         return $this->render('review_tour', [
-            'reviews' => $reviews,
+            'array' => json_encode($array)
         ]);
     }
 }
--- frontend/widgets/views/review_tour.php
+++ frontend/widgets/views/review_tour.php
@@ -1,44 +1,23 @@
 <?php
 
+/* @var $array array */
 
-use yii\helpers\Html;
-use yii\helpers\Url;
-/* @var $reviews \booking\entities\booking\tours\ReviewTour[] */
+$script = <<<JS
+$(document).ready(function() {
+    var i = 0;
+    var arr_reviews = $array;
+    var count = arr_reviews.length;
+    $('#review-load').html(arr_reviews[0]);
+    setInterval(function(){
+            i++;
+            if (i == count) {i=0;}
+         $('#review-load').html(arr_reviews[i]);
+        }, 4000);
+});
+
+JS;
+$this->registerJs($script)
 ?>
+<div id="review-load"></div>
 
-<div class="swiper-viewport">
-    <div id="1slideshow0" class="swiper-container">
-        <div class="swiper-wrapper">
-            <?php foreach ($reviews as $review): ?>
-                <div class="swiper-slide text-center">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div>
-                                    <?= \frontend\widgets\RatingWidget::widget(['rating' => $review->vote]) ?>
-                                </div>
-                                <div class="select-text">
-                                    <?= $review->user->personal->fullname->getFullname() ?>
-                                </div>
-                                <div class="ml-auto">
-                                    <?= date('d-m-Y', $review->created_at) ?>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div class="p-3">
-                                <?= $review->text ?>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
-    <div class="1swiper-pagination 1slideshow0"></div>
-    <div class="1swiper-pager">
-        <div class="1swiper-button-next"></div>
-        <div class="1swiper-button-prev"></div>
-    </div>
-</div>
 
