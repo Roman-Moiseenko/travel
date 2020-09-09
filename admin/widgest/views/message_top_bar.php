8a36b94d1c83e2c6c8fb4b3db200200bec9ef81f
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2020-09-09 21:10:58 +0200

    11. Dialog Complite! Patch!

--- admin/widgest/views/message_top_bar.php
+++ admin/widgest/views/message_top_bar.php
@@ -4,21 +4,26 @@ use booking\entities\booking\BookingItemInterface;
 use booking\helpers\MessageHelper;
 use yii\helpers\Url;
 
+/* @var $dialogs array */
 /* @var $bookings BookingItemInterface */
+
+$count = MessageHelper::countNew();
+if ($count > 0) {$message_count = $count  . ' сообщений';} else {$message_count = 'Новых сообщений нет';}
 ?>
 <li class="nav-item dropdown">
     <a class="nav-link" data-toggle="dropdown" href="#">
         <i class="far fa-envelope"></i>
-        <span class="badge badge-info navbar-badge"><?= MessageHelper::countNew() ?></span>
+        <span class="badge badge-info navbar-badge"><?= $count ?></span>
     </a>
     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-header"><?= MessageHelper::countNew() ?> Сообщений</span>
+        <span class="dropdown-header"><?= $message_count ?></span>
         <div class="dropdown-divider"></div>
         <?php foreach ($dialogs as $id => $item): ?>
         <a href="<?= Url::to(['cabinet/dialog/conversation', 'id' => $id])?>" class="dropdown-item">
             <i class="fas fa-envelope mr-2"></i><span class="badge badge-danger"><?= $item['count'] ?></span> <?= $item['theme'] ?>
         </a>
         <?php endforeach; ?>
+        <a href="<?= Url::to(['cabinet/dialog'])?>" class="dropdown-item">Все сообщения
         </a>
     </div>
 </li>
