4fb21eacf8457600e9038854feab6f3cd02cc437
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2020-12-24 00:21:54 +0200

    Calendar Design Patch!

--- admin/views/tour/calendar/_list_tours.php
+++ admin/views/tour/calendar/_list_tours.php
@@ -9,7 +9,7 @@ use booking\entities\booking\tours\CostCalendar;
 /* @var $errors array */
 ?>
 
-<div class="card card-info" style="max-width: 400px;">
+<div class="card card-info" style="max-width: 400px; width: calc(100vw - 40px)">
     <div id="data-day" data-d="<?= $D ?>" data-m="<?= $M ?>" data-y="<?= $Y ?>"></div>
     <div class="card-header"><span style="font-size: larger; font-weight: bold">На <?= $D ?> число</span></div>
     <div class="card-body p-1">
@@ -24,7 +24,7 @@ use booking\entities\booking\tours\CostCalendar;
             <div class="row">
                 <div class="col-12 calendar-tour-day-info">
                     <i class="fas fa-clock"></i>&nbsp;<?= $costCalendar->time_at ?>&nbsp;
-                    &nbsp;&nbsp;<i class="fas fa-ticket-alt"></i>&nbsp;<?= $costCalendar->tickets ?>&nbsp;
+                    <i class="fas fa-ticket-alt"></i>&nbsp;<?= $costCalendar->tickets ?>&nbsp;
                     <i class="fas fa-ruble-sign"></i>&nbsp;<?= $costCalendar->cost->adult ?>
                     /<?= $costCalendar->cost->child ?? '--' ?>/<?= $costCalendar->cost->preference ?? '--' ?>&nbsp;
                     <a href="#" class="btn del-day" data-id="<?= $costCalendar->id ?>" style="display: inline !important; width: available !important;"><i class="fas fa-times"
