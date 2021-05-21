8e2847c647ea8ae7a89225ba7d7d28935b25c0e8
Roman-Moiseenko <60714951+Roman-Moiseenko@users.noreply.github.com>
2021-05-21 23:46:58 +0200

    Moving +++

--- frontend/views/moving/moving/_image_h3.php
+++ frontend/views/moving/moving/_image_h3.php
@@ -2,11 +2,14 @@
  /* @var $image_file string */
  /* @var $alt string */
 ?>
-
+<div class="row">
+    <div class="col-sm-6">
 <div class="item-responsive item-2-0by1">
     <div class="content-item">
         <img src="<?= \Yii::$app->params['staticHostInfo'] . '/files/images/moving/' . $image_file ?>"
              alt="<?= $alt ?>" title="<?= $alt ?>"
-             width="50%" loading="lazy">
+             width="100%" loading="lazy">
     </div>
+</div>
+</div>
 </div>
