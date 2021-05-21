967b5ab7e5080ce4db48df444ddcd774ea3a8573
Roman-Moiseenko <60714951+Roman-Moiseenko@users.noreply.github.com>
2021-05-21 23:24:23 +0200

    Moving Patch

---
+++ frontend/views/moving/moving/_image_h3.php
@@ -0,0 +1,12 @@
+<?php
+ /* @var $image_file string */
+ /* @var $alt string */
+?>
+
+<div class="item-responsive item-2-0by1">
+    <div class="content-item">
+        <img src="<?= \Yii::$app->params['staticHostInfo'] . '/files/images/moving/' . $image_file ?>"
+             alt="<?= $alt ?>" title="<?= $alt ?>"
+             width="100%" loading="lazy">
+    </div>
+</div>
